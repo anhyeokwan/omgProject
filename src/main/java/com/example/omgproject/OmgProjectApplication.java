bb654bab9d1474b942154b3ec112f1252899bb00
anhyeongkwan <bombom841@naver.com>
2023-12-10 22:34:59 +0900

    Initial commit

---
+++ src/main/java/com/example/omgproject/OmgProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.omgproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmgProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmgProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/omgproject/OmgProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omgproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmgProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
